f374996d77fe36a6d116a40ee4eae9ea33025fee
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2026-07-11 18:57:00 +0200

    Create case study structure

---
+++ src/data/caseStudies.js
@@ -0,0 +1,10 @@
+export const caseStudies = [
+  {
+    slug: "health-insurance-etl",
+    title: "...",
+    overview: "...",
+    problem: "...",
+    solution: "...",
+    results: "...",
+  },
+];
