fb0277b9132963d9b4e821fc99f2cdf5864301fd
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2026-07-11 19:41:08 +0200

    Add development preview route for ETL case study

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
+import { Routes, Route } from "react-router-dom";
+
 import ScrollProgress from "./components/common/ScrollProgress";
 import Navbar from "./components/layout/Navbar";
-import Hero from "./components/sections/Hero";
-import About from "./components/sections/About";
-import Skills from "./components/sections/Skills";
-import Projects from "./components/projects/Projects";
-import Experience from "./components/sections/Experience";
-import Certifications from "./components/sections/Certifications";
-import Education from "./components/sections/Education";
-import Contact from "./components/sections/Contact";
 import Footer from "./components/layout/Footer";
-import BackToTop from "./components/common/BackToTop";  
+import BackToTop from "./components/common/BackToTop";
 
-function App() {
+import Home from "./pages/Home";
+
+import HealthInsuranceETL from "./pages/CaseStudies/HealthInsuranceETL";
+
+export default function App() {
   return (
     <>
       <ScrollProgress />
+
       <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Certifications />
-      <Education />
-      <Contact />
+
+      <Routes>
+
+        {/* ===============================
+            Main Portfolio
+        =============================== */}
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        {/* ===============================
+            Development Preview
+        =============================== */}
+
+        <Route
+          path="/preview/etl"
+          element={<HealthInsuranceETL />}
+        />
+
+      </Routes>
+
       <Footer />
+
       <BackToTop />
     </>
   );
 }
-
-export default App;
